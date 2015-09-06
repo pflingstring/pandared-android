90982407208fc1cfa502e0c3e4743dba187610fc
pflingstring <nzaporojan@gmail.com>
2015-09-07 01:26:01 +0200

    save authentication token to SharedPreferences

--- app/src/main/java/my/fancyapp/LoginActivity.java
+++ app/src/main/java/my/fancyapp/LoginActivity.java
@@ -1,35 +1,83 @@
 package my.fancyapp;
 
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 import android.widget.EditText;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
+
 public class LoginActivity extends Activity
 {
-    public final static String USERNAME = "my.fancyapp.USERNAME";
-    public final static String PASSWORD = "my.fancyapp.PASSWORD";
+    RequestQueue queue; // TODO: make the queue a singleton
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        queue = Volley.newRequestQueue(this);
+
+        // get the authToken or null if not present
+        SharedPreferences authToken = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        String AUTH_TOKEN = authToken.getString("AUTH_TOKEN", null);
+
+        if (AUTH_TOKEN != null)
+        {
+            // BUG   : displays login layout on 'back' click
+            // SHOULD: quit the app
+            Intent mainPage = new Intent(this, MainActivity.class);
+            startActivity(mainPage);
+        }
         setContentView(R.layout.activity_login);
     }
 
-    public void login(View view)
+    public void loginButton(View view)
     {
-        Intent loginIntent = new Intent(this, MainActivity.class);
-
         EditText usernameEditText = (EditText) findViewById(R.id.username);
         EditText passwordEditText = (EditText) findViewById(R.id.password);
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
 
-        loginIntent.putExtra(USERNAME, username);
-        loginIntent.putExtra(PASSWORD, password);
+        makeAuthRequest(username, password);
+    }
 
-        startActivity(loginIntent);
+    void makeAuthRequest(String username, String password)
+    {
+        Response.Listener<String> resListener = new Response.Listener<String>()
+        {
+            @Override
+            public void onResponse(String response)
+            {
+                SharedPreferences authToken = PreferenceManager
+                        .getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor = authToken.edit();
+                editor.putString("AUTH_TOKEN", response);
+                editor.apply();
+
+                Intent mainPage = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainPage);
+            }
+        };
+
+        Response.ErrorListener errListener = new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+            // TODO: handle error
+            }
+        };
+
+        String loginData = "{\"username\" : \""   + username + // refactor
+                           "\", \"password\": \"" + password + "\"}";
+        AuthRequest authRequest = new AuthRequest(loginData, resListener, errListener);
+        queue.add(authRequest);
     }
 }
--- app/src/main/java/my/fancyapp/MainActivity.java
+++ app/src/main/java/my/fancyapp/MainActivity.java
@@ -1,18 +1,13 @@
 package my.fancyapp;
 
-import com.android.volley.toolbox.Volley;
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.Response;
-
-import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 import android.widget.TextView;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
-    RequestQueue queue; // TODO: make the queue a singleton
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -20,39 +15,12 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent loginIntent = getIntent();
-        String username = loginIntent.getStringExtra(LoginActivity.USERNAME);
-        String password = loginIntent.getStringExtra(LoginActivity.PASSWORD);
-        TextView userDetails = (TextView) findViewById(R.id.userDetails);
+        TextView token = (TextView) findViewById(R.id.authToken);
 
-        queue = Volley.newRequestQueue(this);
-        makeRequest(userDetails, username, password);
+        SharedPreferences authToken = PreferenceManager.getDefaultSharedPreferences(this);
+        String AUTH_TOKEN = authToken.getString("AUTH_TOKEN", null);
+
+        token.setText(AUTH_TOKEN);
     }
-
-    void makeRequest(final TextView view, String username, String password)
-    {
-        Response.Listener<String> resListener = new Response.Listener<String>()
-        {
-            @Override
-            public void onResponse(String response)
-            {
-                view.setText(response);
-            }
-        };
-
-        Response.ErrorListener errListener = new Response.ErrorListener()
-        {
-            @Override
-            public void onErrorResponse(VolleyError error)
-            {
-                view.setText("ERROR");
-            }
-        };
-
-        String loginData = "{\"username\" : \"" + username + "\", \"password\": \"" + password + "\"}";
-        AuthRequest authRequest = new AuthRequest(loginData, resListener, errListener);
-        queue.add(authRequest);
-    }
-
 }
 
