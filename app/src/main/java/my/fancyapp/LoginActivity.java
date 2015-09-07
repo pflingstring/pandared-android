1240fd99a4703e68bca02f4dbf548c0eadae08a1
pflingstring <nzaporojan@gmail.com>
2015-09-07 12:34:54 +0200

    fix bug when 'back' click

--- app/src/main/java/my/fancyapp/LoginActivity.java
+++ app/src/main/java/my/fancyapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package my.fancyapp;
 
+import my.fancyapp.requests.AuthRequest;
 import android.preference.PreferenceManager;
 import android.content.SharedPreferences;
 import android.widget.EditText;
@@ -13,9 +14,14 @@ import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.Response;
 
+import android.widget.TextView;
+import org.json.JSONObject;
+
 public class LoginActivity extends Activity
 {
     RequestQueue queue; // TODO: make the queue a singleton
+    public final static String AUTH_TOKEN   = "my.fancy.app.AUTH_TOKEN";
+    public final static String USER_DETAILS = "my.fancy.app.USER_DETAILS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,17 +29,17 @@ public class LoginActivity extends Activity
         super.onCreate(savedInstanceState);
         queue = Volley.newRequestQueue(this);
 
-        // get the authToken or null if not present
-        SharedPreferences authToken = PreferenceManager
+        SharedPreferences sharedPreferences = PreferenceManager
                 .getDefaultSharedPreferences(this);
-        String AUTH_TOKEN = authToken.getString("AUTH_TOKEN", null);
+        String userDetails = sharedPreferences.getString(USER_DETAILS, null);
+        String authToken   = sharedPreferences.getString(AUTH_TOKEN, null);
 
-        if (AUTH_TOKEN != null)
+        if (authToken != null)
         {
-            // BUG   : displays login layout on 'back' click
-            // SHOULD: quit the app
-            Intent mainPage = new Intent(this, MainActivity.class);
-            startActivity(mainPage);
+            setContentView(R.layout.activity_main);
+            TextView userDetailsView = (TextView) findViewById(R.id.userDetails);
+            userDetailsView.setText(userDetails);
+            return;
         }
         setContentView(R.layout.activity_login);
     }
@@ -50,18 +56,20 @@ public class LoginActivity extends Activity
 
     void makeAuthRequest(String username, String password)
     {
-        Response.Listener<String> resListener = new Response.Listener<String>()
+        Response.Listener<JSONObject> resListener = new Response.Listener<JSONObject>()
         {
             @Override
-            public void onResponse(String response)
+            public void onResponse(JSONObject response)
             {
-                SharedPreferences authToken = PreferenceManager
+                SharedPreferences sharedPreferences = PreferenceManager
                         .getDefaultSharedPreferences(getApplicationContext());
-                SharedPreferences.Editor editor = authToken.edit();
-                editor.putString("AUTH_TOKEN", response);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(AUTH_TOKEN, AuthRequest.getAuthToken(response));
+                editor.putString(USER_DETAILS, AuthRequest.getUserDetails(response));
                 editor.apply();
 
                 Intent mainPage = new Intent(getApplicationContext(), MainActivity.class);
+                mainPage.putExtra(USER_DETAILS, AuthRequest.getUserDetails(response));
                 startActivity(mainPage);
             }
         };
--- app/src/main/java/my/fancyapp/MainActivity.java
+++ app/src/main/java/my/fancyapp/MainActivity.java
@@ -1,26 +1,22 @@
 package my.fancyapp;
 
-import android.preference.PreferenceManager;
-import android.content.SharedPreferences;
 import android.widget.TextView;
+import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
-
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView token = (TextView) findViewById(R.id.authToken);
-
-        SharedPreferences authToken = PreferenceManager.getDefaultSharedPreferences(this);
-        String AUTH_TOKEN = authToken.getString("AUTH_TOKEN", null);
-
-        token.setText(AUTH_TOKEN);
+        Intent intent = getIntent();
+        String userDetails = intent.getStringExtra(LoginActivity.USER_DETAILS);
+        TextView userDetailsView = (TextView) findViewById(R.id.userDetails);
+        userDetailsView.setText(userDetails);
     }
 }
 
