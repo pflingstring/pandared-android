95d181ecaed7c442aa3a2a2c2cc7c2480515a3fd
pflingstring <nzaporojan@gmail.com>
2015-09-12 13:28:26 +0200

    add PM IDs to ListView

--- app/src/main/java/red/panda/ConversationActivity.java
+++ app/src/main/java/red/panda/ConversationActivity.java
@@ -3,18 +3,25 @@ package red.panda;
 import red.panda.requests.ConversationRequest;
 import red.panda.utils.RequestQueueSingleton;
 import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.content.Context;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.Response;
 
-import android.content.SharedPreferences;
-import android.content.Context;
-import android.widget.TextView;
-import android.app.Activity;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +29,7 @@ import java.util.Map;
 public class ConversationActivity extends Activity
 {
     RequestQueue queue;
-    TextView conversationView;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,7 +38,16 @@ public class ConversationActivity extends Activity
         setContentView(R.layout.activity_conversation);
 
         queue = RequestQueueSingleton.getQueue(this);
-        conversationView = (TextView) findViewById(R.id.pm);
+        listView = (ListView) findViewById(R.id.pmIDList);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Toast toast = Toast.makeText(getApplicationContext(), "you clicked me HAHA lol", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
         makePMRequest("");
     }
 
@@ -42,7 +58,17 @@ public class ConversationActivity extends Activity
             @Override
             public void onResponse(String response)
             {
-                conversationView.setText(response);
+                try
+                {
+                    String[] ids = getConversationIDs(response);
+                    ArrayAdapter<String> idAdapter = new ArrayAdapter<String>(
+                            getApplicationContext(), android.R.layout.simple_list_item_1, ids);
+                    listView.setAdapter(idAdapter);
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
             }
         };
 
@@ -58,8 +84,7 @@ public class ConversationActivity extends Activity
             }
         };
 
-        ConversationRequest request = new ConversationRequest(
-                ConversationRequest.URL, resListener, errListener)
+        ConversationRequest request = new ConversationRequest(resListener, errListener)
         {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError
@@ -74,4 +99,20 @@ public class ConversationActivity extends Activity
         };
         RequestQueueSingleton.addToQueue(request, this);
     }
+
+    static String[] getConversationIDs(String jsonResponse) throws JSONException
+    {
+        JSONArray jsonArray = new JSONObject(jsonResponse).getJSONArray("data");
+        int length = jsonArray.length();
+        String[] result = new String[length];
+
+        for (int i=0; i<length; i++)
+        {
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            String id = jsonObj.getString("id");
+            result[i] = id;
+        }
+        return result;
+    }
+
 }
