921c7e63b9d0daa86ccb22c132a452989dba7c4d
pflingstring <nzaporojan@gmail.com>
2015-09-18 01:10:03 +0300

    add click listener on NavigationDrawer item

--- app/src/main/java/red/panda/LoginActivity.java
+++ app/src/main/java/red/panda/LoginActivity.java
@@ -5,7 +5,10 @@ import red.panda.utils.RequestQueueSingleton;
 import red.panda.requests.AuthRequest;
 import red.panda.utils.Constants;
 
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import com.android.volley.VolleyError;
@@ -63,34 +66,70 @@ public class LoginActivity extends AppCompatActivity implements FragmentDrawer.F
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        drawerFragment = (FragmentDrawer)
-                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+        drawerFragment = (FragmentDrawer) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
         drawerFragment.setDrawerListener(this);
 
     }
 
     @Override
-    public void onDrawerItemSelected(View view, int position) {
+    public void onDrawerItemSelected(View view, int position)
+    {
+        displayView(position);
+    }
 
+    private void displayView(int position)
+    {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position)
+        {
+            case 0:
+                fragment = new HomeFragment();
+                title = getString(R.string.app_name);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null)
+        {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        if (id == R.id.action_search)
+        {
+            Toast.makeText(getApplicationContext(), "Search action is selected!", Toast.LENGTH_SHORT).show();
             return true;
         }
 
