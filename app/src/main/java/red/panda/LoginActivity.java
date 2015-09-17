0e0e77f3350ef42c0011a464d2de8d37de33d6d8
pflingstring <nzaporojan@gmail.com>
2015-09-17 13:40:57 +0300

    add toolbar to LoginActivity

--- app/src/main/java/red/panda/LoginActivity.java
+++ app/src/main/java/red/panda/LoginActivity.java
@@ -1,41 +1,41 @@
 package red.panda;
 
-import red.panda.utils.Constants;
 import red.panda.utils.RequestQueueSingleton;
 import red.panda.requests.AuthRequest;
+import red.panda.utils.Constants;
 
-import android.content.Context;
-import android.preference.PreferenceManager;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.widget.EditText;
-import android.content.Intent;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-
-import com.android.volley.RequestQueue;
+import android.support.v7.widget.Toolbar;
 import com.android.volley.VolleyError;
 import com.android.volley.Response;
 
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import android.view.MenuItem;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.View;
+
+import android.content.Intent;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity
 {
-    RequestQueue queue;
     public final static String AUTH_TOKEN   = "red.panda.AUTH_TOKEN";
     public final static String USER_DETAILS = "red.panda.USER_DETAILS";
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        queue = RequestQueueSingleton.getQueue(this);
 
         SharedPreferences sharedPreferences = PreferenceManager
                 .getDefaultSharedPreferences(getApplicationContext());
@@ -53,6 +53,34 @@ public class LoginActivity extends AppCompatActivity
             return;
         }
         setContentView(R.layout.activity_login);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void loginButton(View view)
