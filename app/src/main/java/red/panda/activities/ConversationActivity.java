3d2f5b9e0fcb1fd08ee3394642b72bc1ce16e2f2
pflingstring <nzaporojan@gmail.com>
2015-10-24 03:32:33 +0200

    experimental java programming style

--- app/src/main/java/red/panda/activities/ConversationActivity.java
+++ app/src/main/java/red/panda/activities/ConversationActivity.java
@@ -1,24 +1,23 @@
 package red.panda.activities;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.inputmethod.InputMethodManager;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import red.panda.R;
 import red.panda.activities.fragments.ConversationFragment;
 import red.panda.activities.fragments.FragmentDrawer;
 import red.panda.utils.FragmentUtils;
 import red.panda.utils.JsonUtils;
+import red.panda.R;
+
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.widget.DrawerLayout;
+import android.view.inputmethod.InputMethodManager;
+import android.view.MenuItem;
+import android.view.Menu;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.Intent;
+import android.app.Activity;
+import android.os.Bundle;
 
 public class ConversationActivity extends AppCompatActivity
 {
@@ -31,39 +30,80 @@ public class ConversationActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R
+                .id.toolbar
+        );
         setSupportActionBar(toolbar);
+
         if (getSupportActionBar() != null)
         {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            getSupportActionBar().setTitle("Conversations");
+            getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(false)
+            ;
+            getSupportActionBar()
+                .setTitle("Conversations")
+            ;
         }
 
         drawerFragment = (FragmentDrawer) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+            .findFragmentById(R.id
+                .fragment_navigation_drawer
+        );
+
+        drawerFragment.setUp(R.id
+            .fragment_navigation_drawer
+            , (DrawerLayout) findViewById(R.id
+                .drawer_layout)
+            , toolbar
+        );
 
         Intent intent = getIntent();
         JSONObject json = null;
+
         try
         {
-            json = new JSONObject(intent.getStringExtra("MSG_ID"));
+            json = new JSONObject(intent
+                .getStringExtra("MSG_ID")
+            );
         }
         catch (JSONException e)
         {
             e.printStackTrace();
         }
-        String id = JsonUtils.getFieldFromJSON(json, "id");
-        String username = JsonUtils.getFieldFromJSON(json, "username");
-        ConversationFragment fragment = ConversationFragment.newInstance(id, username);
-        FragmentUtils.replaceFragmentWith(fragment, this, false);
+
+        String id = JsonUtils
+            .getFieldFromJSON(
+                json
+                , "id"
+        );
+
+        String username = JsonUtils
+            .getFieldFromJSON(
+                json
+                , "username"
+        );
+
+        ConversationFragment fragment = ConversationFragment
+            .newInstance(
+                id
+                , username
+        );
+
+        FragmentUtils.replaceFragmentWith(
+            fragment
+            , this
+            , false
+        );
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_conversation, menu);
+        getMenuInflater().inflate(R.menu
+            .menu_conversation
+            , menu
+        );
         return true;
     }
 
@@ -72,7 +112,7 @@ public class ConversationActivity extends AppCompatActivity
     {
         ActionBarDrawerToggle drawToggle = drawerFragment.getDrawerToggle();
         if (drawToggle.isDrawerIndicatorEnabled() &&
-                drawToggle.onOptionsItemSelected(item))
+            drawToggle.onOptionsItemSelected(item))
         { return true; }
 
         switch (item.getItemId())
@@ -92,19 +132,25 @@ public class ConversationActivity extends AppCompatActivity
 
         if (getSupportActionBar() != null)
         {
-            getSupportActionBar().setTitle("Conversations");
-            getSupportActionBar().setIcon(android.R.color.transparent);
+            getSupportActionBar()
+                .setTitle("Conversations");
+            getSupportActionBar()
+                .setIcon(android.R.color.transparent);
         }
 
         // hide soft keyboard
         InputMethodManager inputMethodManager = (InputMethodManager)
-                this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            this
+            .getSystemService(Activity.INPUT_METHOD_SERVICE)
+        ;
+
         if (getCurrentFocus() != null)
-        {
-            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-        }
+            inputMethodManager.hideSoftInputFromWindow(
+                getCurrentFocus()
+                .getWindowToken()
+                , 0
+            );
 
         drawerFragment.setDrawerToggle(true);
     }
-
 }
